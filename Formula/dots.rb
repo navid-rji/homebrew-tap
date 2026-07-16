3145878e05b15800a2d114949419de77a2f1875f
Navid Rajaei <112384620+navid-rji@users.noreply.github.com>
2026-07-16 15:34:36 +0000

    dots: add 0.2.0 bottle.

--- Formula/dots.rb
+++ Formula/dots.rb
@@ -6,6 +6,12 @@ class Dots < Formula
   license "MIT"
   head "https://github.com/navid-rji/dots.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/navid-rji/homebrew-tap/releases/download/dots-0.2.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "1508dfea702e7d4d8f8519f48dbd8bfa406d5177ae408c16ad26de848402ac99"
+    sha256 cellar: :any,                 x86_64_linux: "3f5ed1a4df397250839e1b05bd4fade98e7241dd69776884757c0b7ea7ddc880"
+  end
+
   depends_on "go" => :build
 
   def install
