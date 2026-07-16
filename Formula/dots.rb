ce280ff51a50901c57184fc28d600f7438021d54
navidrajaei <navid.rajaei@tum.de>
2026-07-16 17:24:23 +0200

    dots 0.2.0

--- Formula/dots.rb
+++ Formula/dots.rb
@@ -1,16 +1,10 @@
 class Dots < Formula
   desc "Tiny CLI dotfile manager"
   homepage "https://github.com/navid-rji/dots"
-  url "https://github.com/navid-rji/dots/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "d8a931cb6d91635b7602109ab4dabde16c463f8e7832339dbfa92063206682ca"
+  url "https://github.com/navid-rji/dots/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "0d4f3c4f5b20626c6a74d8dbab194f8758e57cc1191b8afe4e5ea43bf252587f"
   license "MIT"
   head "https://github.com/navid-rji/dots.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/navid-rji/homebrew-tap/releases/download/dots-0.1.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "b91443102d6678d78177cbaff465d56700720301273f92e363828a164e543819"
-    sha256 cellar: :any,                 x86_64_linux: "5fa27c96079ffbeb0d94e726c8c346ea42db433c22b8e4e00bc15010624446d6"
-  end
 
   depends_on "go" => :build
 
@@ -20,6 +14,7 @@ class Dots < Formula
       -X github.com/navid-rji/dots/internal/buildinfo.version=#{version}
     ]
     system "go", "build", *std_go_args(ldflags: ldflags)
+    generate_completions_from_executable(bin/"dots", shell_parameter_format: :cobra, shells: [:bash, :zsh, :fish])
   end
 
   test do
