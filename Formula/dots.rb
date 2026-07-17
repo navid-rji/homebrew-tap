40b825120c148b23955bbc89af2b312c9925837f
navidrajaei <navid.rajaei@tum.de>
2026-07-17 14:05:37 +0200

    dots 0.3.0
    Closes #3.
    
    Signed-off-by: Navid Rajaei <112384620+navid-rji@users.noreply.github.com>

--- Formula/dots.rb
+++ Formula/dots.rb
@@ -1,16 +1,10 @@
 class Dots < Formula
   desc "Tiny CLI dotfile manager"
   homepage "https://github.com/navid-rji/dots"
-  url "https://github.com/navid-rji/dots/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "0d4f3c4f5b20626c6a74d8dbab194f8758e57cc1191b8afe4e5ea43bf252587f"
+  url "https://github.com/navid-rji/dots/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "d2581675c0a1b06edbb18725dbab7857ed76b14ca42bad807a3e905b2fc14050"
   license "MIT"
   head "https://github.com/navid-rji/dots.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/navid-rji/homebrew-tap/releases/download/dots-0.2.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "1508dfea702e7d4d8f8519f48dbd8bfa406d5177ae408c16ad26de848402ac99"
-    sha256 cellar: :any,                 x86_64_linux: "3f5ed1a4df397250839e1b05bd4fade98e7241dd69776884757c0b7ea7ddc880"
-  end
 
   depends_on "go" => :build
 
